75492fe87b89688b5c7fbeb6769d395949690252
Ivan Duran <idurandiaz@gmail.com>
2019-02-16 12:41:28 +0500

    Create signin recover and request routes

--- src/router.js
+++ src/router.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
+import Signin from './views/SignInFlow/Signin';
+import Request from './views/SignInFlow/Request';
+import Recover from './views/SignInFlow/Recover';
 
 Vue.use(Router);
 
@@ -18,6 +21,21 @@ export default new Router({
             path: '/about',
             name: 'about',
             component: About
-        }
+        },
+        {
+            path: '/signin',
+            name: 'signin',
+            component: Signin
+        },
+        {
+            path: '/request',
+            name: 'request',
+            component: Request
+        },
+        {
+            path: '/recover',
+            name: 'recover',
+            component: Recover
+        },
     ]
 });
---
+++ src/views/SignInFlow/Signin.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container"></div>
+</template>
+
+<script>
+export default {
+    name: "Signin"
+};
+</script>
+
+<style scoped lang="scss">
+</style>
