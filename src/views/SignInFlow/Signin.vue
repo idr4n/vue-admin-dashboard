7c861efbfac59bd2cf9a800ff399dc9d67e2f6a9
Ivan Duran <idurandiaz@gmail.com>
2019-02-16 14:14:00 +0500

    Finish signin view

--- src/views/SignInFlow/Signin.vue
+++ src/views/SignInFlow/Signin.vue
@@ -1,5 +1,17 @@
 <template>
-    <div class="container"></div>
+    <div class="container">
+        <div class="request">Don't have a Dashboard account?
+            <router-link to="/request">Request an account</router-link>
+        </div>
+        <div class="login">
+            <img src="@/assets/logo-light.svg" alt="logo">
+            <h4>Sign into the Dashboard</h4>
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Password">
+            <button>Sign In</button>
+            <router-link to="/recover">Forgot your password?</router-link>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,4 +21,79 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    min-height: 100vh;
+}
+
+.login {
+    width: 400px;
+}
+
+h4 {
+    margin: 0;
+    margin-top: 20px;
+    line-height: 34px;
+    font-size: 24px;
+    text-align: center;
+    color: #ffffff;
+}
+
+input {
+    background: rgba(255, 255, 255, 0.2);
+    border: 1px solid rgba(255, 255, 255, 0.2);
+    box-sizing: border-box;
+    border-radius: 4px;
+
+    height: 60px;
+    width: 100%;
+
+    font-size: 20px;
+    color: white;
+    padding-left: 20px;
+    margin-top: 20px;
+
+    &::placeholder {
+        color: rgba(255, 255, 255, 0.3);
+    }
+}
+
+button {
+    background: #56ccf2;
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+    border-radius: 4px;
+    border: none;
+
+    height: 60px;
+    width: 100%;
+
+    font-size: 20px;
+    color: white;
+    margin-top: 20px;
+    margin-bottom: 40px;
+}
+
+a {
+    line-height: 25px;
+    font-size: 16px;
+    text-align: center;
+
+    color: rgba(255, 255, 255, 0.3);
+    text-decoration: none;
+}
+
+.request {
+    position: absolute;
+    top: 40px;
+    right: 40px;
+
+    color: rgba(255, 255, 255, 0.3);
+
+    a {
+        color: white;
+    }
+}
 </style>
