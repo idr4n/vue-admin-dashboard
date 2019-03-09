5e9447e5f8f5450a4936d668b16daa76ad859c85
Ivan Duran <idurandiaz@gmail.com>
2019-03-09 07:06:04 +0500

    Add netlify widget, gotrue, signout notification

---
+++ src/components/Notification.vue
@@ -0,0 +1,48 @@
+<template>
+    <transition
+        enter-active-class="animated fadeInDown"
+        leave-active-class="animated fadeOutRight"
+    >
+        <div v-show="show" class="bubble">
+            <div class="message">{{ text }}</div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    name: "Notification",
+    props: {
+        text: String
+    },
+    data() {
+        return {
+            show: false
+        };
+    },
+    mounted() {
+        this.show = true;
+        setTimeout(() => {
+            this.show = false;
+        }, 3000);
+    }
+};
+</script>
+
+<style scoped lang="scss">
+.bubble {
+    border-radius: 6px;
+    position: absolute;
+    top: 5%;
+    padding: 5px 10px 5px 10px;
+    background: $blue;
+}
+
+.message {
+    @include medium-text;
+    color: $white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
--- src/views/SignInFlow/Signin.vue
+++ src/views/SignInFlow/Signin.vue
@@ -6,6 +6,7 @@
             'dark-background': isDarkMode
         }"
     >
+        <Notification v-if="hasText" :text="text"/>
         <RequestAccount/>
         <div class="login">
             <img src="@/assets/logo-light.svg" alt="logo" v-show="isDarkMode">
@@ -13,17 +14,23 @@
             <h4
                 :class="{ 'light-text': isDarkMode, 'dark-text': !isDarkMode }"
             >Sign into the Dashboard</h4>
-            <input
-                type="email"
-                placeholder="Email"
-                :class="{'light-field': isDarkMode, 'dark-field': !isDarkMode}"
-            >
-            <input
-                type="password"
-                placeholder="Password"
-                :class="{'light-field': isDarkMode, 'dark-field': !isDarkMode}"
-            >
-            <button>Sign In</button>
+            <form @submit.prevent="onSubmit" action>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    :class="{'light-field': isDarkMode, 'dark-field': !isDarkMode}"
+                    v-model="email"
+                    required
+                >
+                <input
+                    type="password"
+                    placeholder="Password"
+                    :class="{'light-field': isDarkMode, 'dark-field': !isDarkMode}"
+                    v-model="password"
+                    required
+                >
+                <button>Sign In</button>
+            </form>
             <router-link
                 to="/recover"
                 :class="{'light-link': isDarkMode, 'dark-link': !isDarkMode}"
@@ -36,16 +43,47 @@
 <script>
 import RequestAccount from "@/components/RequestAccount";
 import ThemeSwitch from "@/components/ThemeSwitch";
+import Notification from "@/components/Notification";
+import { auth } from "@/main";
 
 export default {
     name: "Signin",
+    data() {
+        return {
+            email: null,
+            password: null,
+            hasText: false,
+            text: ""
+        };
+    },
     components: {
         RequestAccount,
-        ThemeSwitch
+        ThemeSwitch,
+        Notification
     },
     computed: {
         isDarkMode() {
             return this.$store.getters.isDarkMode;
+        }
+    },
+    methods: {
+        onSubmit() {
+            const email = this.email;
+            const password = this.password;
+
+            auth.login(email, password, true)
+                .then(response => {
+                    this.$router.replace("/");
+                })
+                .catch(error => alert("Error: " + error));
+        }
+    },
+    mounted() {
+        const params = this.$route.params;
+
+        if (params.userLoggedOut) {
+            this.hasText = true;
+            this.text = "You have logged out!";
         }
     }
 };
